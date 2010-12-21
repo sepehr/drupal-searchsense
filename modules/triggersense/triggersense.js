5374d0d3ff517a6524e31b3c61454f25844cb6f9
Sepehr Lajevardi <me@sepehr.ws>
2010-12-21 06:25:42 +0330

    Implemented required JS behaviors for Triggersense.

---
+++ modules/triggersense/triggersense.js
@@ -0,0 +1,65 @@
+// $Id$
+
+/**
+ * @file
+ * Searchsense Trigger JS behaviors.
+ */
+
+/**
+ * Defines triggersense mother object.
+ */
+Drupal.triggersense = Drupal.triggersense || {};
+
+
+/**
+ * Helper method to perform a delayed form submission.
+ *
+ * @param $form
+ *   jQuery object of target form.
+ */
+Drupal.triggersense.delayedSubmit = function($form) {
+  window.setTimeout(function() {
+    $form
+      .find('.form-submit[type="submit"]')
+      .trigger('click');
+  }, 200);
+
+  return false;
+};
+
+
+/**
+ * Alters autocomplete fields behavior to accept Click/Enter events.
+ */
+Drupal.behaviors.triggersense = function(context) {
+  $('.form-autocomplete').each(function() {
+    // Preserve this.
+    var $this = $(this);
+
+    // Enter key submission trigger.
+    $this.unbind('keypress')
+      .bind('keypress', function(e) {
+        if (e.keyCode == 13) {
+          Drupal.triggersense.delayedSubmit($this.parent('form'));
+        }
+      });
+
+    // Click submission trigger.
+    $this.parent('form')
+      .find('#autocomplete li')
+      .bind('click', function(e) {
+        Drupal.triggersense.delayedSubmit($this.parent('form'));
+      });
+  });
+};
+
+
+/**
+ * Binds the triggersense behavior where required.
+ */
+Drupal.behaviors.triggersenseBinder = function(context) {
+  $(document).bind('ajaxComplete', function() {
+    Drupal.behaviors.triggersense(context);
+  });
+};
+
